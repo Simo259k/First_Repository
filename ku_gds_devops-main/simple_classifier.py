ed6959767dfeccabc82ef88bebfd2d0981c356ac
simon henriksen <simonhenriksen2010.sh.sh@gmail.com>
2025-02-06 15:38:05 +0100

    added simple_classifier.py

--- ku_gds_devops-main/simple_classifier.py
+++ ku_gds_devops-main/simple_classifier.py
@@ -7,6 +7,8 @@ from sklearn.inspection import DecisionBoundaryDisplay
 if __name__ == "__main__":
     n_neighbors = 15
 
+    skr
+    
     # import some data to play with
     iris = datasets.load_iris()
 
